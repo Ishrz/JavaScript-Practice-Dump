8a7028e7ff5a1c2c6153634e5156039de91eecda
Shaykh Ahamad <ishrzz66@gmail.com>
2025-09-18 14:00:35 +0530

    array practice with methods

---
+++ funassifnment13.js
@@ -0,0 +1,86 @@
+// let fruit = ["apple", "guvua", "banana"];
+
+// fruit.splice(1,0, "Red", "blue");
+// console.log(fruit);
+
+// let item = [1,2,3,4,5,6];
+// let newitem= item.slice(2,5);
+// console.log(item);
+// console.log(newitem);
+
+// let names = [ "zara", "arjun", "mira", "bhavya"];
+// names.sort().reverse();
+// console.log(names);
+
+// let arr = [1,2,3,4];
+// let newarr = arr.map( function(val){
+//     return val*val;
+// });
+// console.log(newarr);
+
+// let arr = [5,12,8,20,3];
+// let newarr = arr.filter( function(val){
+//     return val > 10;
+// });
+// console.log(newarr);
+
+// let newarr2 = arr.filter( (val) => {
+//     return val > 10;
+// });
+// console.log(newarr2);
+
+// let arr = [ 10,20,30];
+//  let newarrsum= arr.reduce(function(accu, val){
+//     return accu + val;
+//  }, 0 );
+//  console.log(newarrsum);
+
+//  let newarrsum2 = arr.reduce( (accu,val) => {
+//     return accu + val;
+//  },0);
+//  console.log(newarrsum2);
+
+
+// let arr = [12,15,3,8,20];
+// let arrnew = arr.find( (val) =>{
+//     return val < 10;
+// });
+// console.log(arrnew);
+
+// let arr = [45, 60, 28, 90];
+// let newarr = arr.some( (val) =>{
+//     return val < 35;
+// });
+// console.log(newarr);
+
+
+// let arr = [ 2,4,6,8,10];
+
+// let newarr = arr.every( (val) =>{
+//     return (val%2) === 0; 
+// });
+// console.log(newarr);
+
+// let fullName= ["Harsh", "sharma"];
+// let [firsName, lastName] = fullName;
+// console.log(firsName);
+// console.log(lastName);
+// console.log(fullName);
+
+
+// let a = [1,2];
+// let b = [3,4];
+
+// let c = [...a , ...b];
+// console.log(c);
+
+// let countries = ["USA", "UK"];
+// countries = ["India", ...countries];
+// console.log(countries);
+
+// let arr = [1,2,3];
+// let arr2 = [ ...arr];
+// console.log(arr);
+// console.log(arr2);
+// arr2.pop();
+// console.log(arr2);
