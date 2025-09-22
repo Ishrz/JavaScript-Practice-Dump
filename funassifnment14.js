47af2fecdcba817eb9e17f12bc8093ef2c4c7ce8
Shaykh Ahamad <ishrzz66@gmail.com>
2025-09-22 16:38:26 +0530

    html/css practice dump

---
+++ funassifnment14.js
@@ -0,0 +1,16 @@
+//singleton
+//object create
+
+//objects literals
+
+const JsUser = {
+    name : "hitesh",
+    age : 18,
+    location : "jaipur",
+    email: "zyx@gmail.com" 
+}
+
+console.log(JsUser.name);
+
+console.log(" ");
+
