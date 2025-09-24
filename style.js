5deb5e7c2cd48809b1479a1aa9ac76505ba2e93f
Shaykh Ahamad <ishrzz66@gmail.com>
2025-09-24 15:30:59 +0530

    DOM text mainuplation

--- style.js
+++ style.js
@@ -1,4 +1,20 @@
-let abc = document.getElementById("x");
-console.log(abc);
+// let abc = document.getElementById("x");
+// console.log(abc);
+
+
+// let h1Tag= document.querySelector("h1");
+// h1Tag.textContent = "textContent QS"
+
+// let hTag = document.querySelectorAll("h1");
+// hTag[1].innerHTML = " inner Text" ;
+
+// hTag[0].innerHTML = "first H1 tag";
+// hTag[2].textContent = "3rd H1 tag";
+
+
+
+
+
+
 
 
