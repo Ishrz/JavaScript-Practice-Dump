9d33fd78a33d23e91e6060de552ccebfe784eee9
Shaykh Ahamad <ishrzz66@gmail.com>
2025-09-24 16:18:51 +0530

    Dom Create elements/remove/append/prepend and changing css style via .style

--- style.js
+++ style.js
@@ -11,11 +11,27 @@
 // hTag[0].innerHTML = "first H1 tag";
 // hTag[2].textContent = "3rd H1 tag";
 
-let attri = document.querySelector("a");
+// let attri = document.querySelector("a");
 // attri.setAttribute("href", "https://www.google.com");
 
 // console.log(attri.getAttribute("href"));
 
 // attri.removeAttribute("href");
+
+// //element create hua
+// let abc = document.createElement("h2");
+// //element me value diye
+// abc.textContent = "Hello from created h2 ";
+// //element add kare/show kare
+// //first way...
+// // document.body.prepend(abc);
+// //second way of doing it.
+// document.querySelector("body").append(abc);
+
+
+// let abc = document.querySelector("h1");
+// console.dir(abc);
+// abc.style.color = "red"; 
+// abc.style.backgroundColor = "blue"
 
 
