cdc949e07146c2650341efaa15503a085075f1d3
Shaykh Ahamad <ishrzz66@gmail.com>
2025-09-25 16:36:54 +0530

    EL Input event

--- style2.js
+++ style2.js
@@ -1,11 +1,19 @@
-let h2 = document.querySelector("h2");
-h2.addEventListener("click", function(){
-    h2.style.color = "red";
+// let h2 = document.querySelector("h2");
+// h2.addEventListener("click", function(){
+//     h2.style.color = "red";
 
-})
+// })
 
-document.querySelector("p")
-.addEventListener("click", function(){
-                this.textContent = "Surprisse hahahah";
-                this.style.color = "red";
+// document.querySelector("p")
+// .addEventListener("click", function(){
+//                 this.textContent = "Surprisse hahahah";
+//                 this.style.color = "red";
+// })
+
+let ip = document.querySelector("input");
+ip.addEventListener("input", function(evt){
+    if(evt.data !== null){
+        console.log(evt.data);
+   }
+    
 })
