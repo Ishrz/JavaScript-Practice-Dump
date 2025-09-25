fc078239594727507ac7715d38d99961a70b6cee
Shaykh Ahamad <ishrzz66@gmail.com>
2025-09-26 00:35:35 +0530

    Button EL Created

--- style3.js
+++ style3.js
@@ -1,12 +1,28 @@
 
-let h1 =document.querySelector("h1");
-window.addEventListener("keydown", function(evt){
-    console.log(evt.key);
-    if(evt.key === " "){
-        h1.textContent = "SPACE";
-    }
-    else {
-        h1.textContent = evt.key
-    }
+// let h1 =document.querySelector("h1");
+// window.addEventListener("keydown", function(evt){
+//     console.log(evt.key);
+//     if(evt.key === " "){
+//         h1.textContent = "SPACE";
+//     }
+//     else {
+//         h1.textContent = evt.key
+//     }
     
+// })
+
+
+
+let btn = document.querySelector("#btn");
+let ipt = document.querySelector("#ipt");
+
+btn.addEventListener("click", function(){
+    ipt.click();
+});
+
+ipt.addEventListener("change", function(evt){
+    const file = evt.target.files[0];
+    if(file){
+        btn.textContent = file.name;
+    }
 })
