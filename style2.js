b7727413e39e5d09cbaa19d19fa75ca3c09a4e76
Shaykh Ahamad <ishrzz66@gmail.com>
2025-09-25 16:00:45 +0530

    content changing on clicking

--- style2.js
+++ style2.js
@@ -3,3 +3,9 @@ h2.addEventListener("click", function(){
     h2.style.color = "red";
 
 })
+
+document.querySelector("p")
+.addEventListener("click", function(){
+                this.textContent = "Surprisse hahahah";
+                this.style.color = "red";
+})
