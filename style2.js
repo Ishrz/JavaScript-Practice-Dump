eb6843775adc44778636653e073d982ce0d4ef9c
Shaykh Ahamad <ishrzz66@gmail.com>
2025-09-25 19:04:49 +0530

    EL change

--- style2.js
+++ style2.js
@@ -10,10 +10,22 @@
 //                 this.style.color = "red";
 // })
 
-let ip = document.querySelector("input");
-ip.addEventListener("input", function(evt){
-    if(evt.data !== null){
-        console.log(evt.data);
-   }
+// let ip = document.querySelector("input");
+// ip.addEventListener("input", function(evt){
+//     if(evt.data !== null){
+//         console.log(evt.data);
+//    }
+    
+// })
+
+
+let slc = document.querySelector("select");
+ let h2= document.querySelector("h2");
+
+slc.addEventListener("change", function(evt){
+h2.textContent= `${evt.target.value} is selected`;
     
 })
+
+
+
