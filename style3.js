717edaca728241a9224849aac1477feb09cfd674
Shaykh Ahamad <ishrzz66@gmail.com>
2025-09-27 02:35:36 +0530

    Event Listener : MouseOver and MouseOut

--- style3.js
+++ style3.js
@@ -27,50 +27,59 @@
 //     }
 // });
 
-let form = document.querySelector("form")
-let inputs = document.querySelectorAll("input");
-let main = document.querySelector("#main");
+// let form = document.querySelector("form")
+// let inputs = document.querySelectorAll("input");
+// let main = document.querySelector("#main");
 
-form.addEventListener("submit", function(evt){
-    evt.preventDefault();
+// form.addEventListener("submit", function(evt){
+//     evt.preventDefault();
 
-    let card = document.createElement("div");
-    card.classList.add("card");
+//     let card = document.createElement("div");
+//     card.classList.add("card");
  
-    let profile = document.createElement("div");
-    profile.classList.add("profile");
+//     let profile = document.createElement("div");
+//     profile.classList.add("profile");
 
-    let img = document.createElement("img");
-    // img.setAttribute("src", "https://images.unsplash.com/photo-1729106657786-ef318f75e365?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8");
-    img.setAttribute("src", inputs[0].value)
+//     let img = document.createElement("img");
+//     // img.setAttribute("src", "https://images.unsplash.com/photo-1729106657786-ef318f75e365?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8");
+//     img.setAttribute("src", inputs[0].value)
 
-    let h3 = document.createElement("h3");
-    let h5 = document.createElement("h5"); 
-    let p = document.createElement("p");
+//     let h3 = document.createElement("h3");
+//     let h5 = document.createElement("h5"); 
+//     let p = document.createElement("p");
 
-    h3.textContent = inputs[1].value;
-    h5.textContent = inputs[2].value;
-    p.textContent = inputs[3].value;
-
-
-
-    profile.appendChild(img);
-    card.appendChild(profile);
-    card.appendChild(h3);
-    card.appendChild(h5);
-    card.appendChild(p);
+//     h3.textContent = inputs[1].value;
+//     h5.textContent = inputs[2].value;
+//     p.textContent = inputs[3].value;
 
 
-    main.appendChild(card);
 
-    inputs.forEach(function(inp){
-        if(inp.type !== "submit"){
-            inp.value = "";
+//     profile.appendChild(img);
+//     card.appendChild(profile);
+//     card.appendChild(h3);
+//     card.appendChild(h5);
+//     card.appendChild(p);
 
-        }
+
+//     main.appendChild(card);
+
+//     inputs.forEach(function(inp){
+//         if(inp.type !== "submit"){
+//             inp.value = "";
+
+//         }
+//     })
+
+
+
+let mouse = document.querySelector("#m1");
+    mouse.addEventListener("mouseover", function(){
+        mouse.style.backgroundColor = "yellow";
+    })
+    mouse.addEventListener("mouseout", function(){
+        mouse.style.backgroundColor ="red";
     })
 
-    
 
 
 
@@ -84,6 +93,3 @@ form.addEventListener("submit", function(evt){
 
 
 
-
-
-})
