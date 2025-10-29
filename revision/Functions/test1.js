97ade7fd78b3197a5e3e7c90eb2d16d6ad4dfaa4
Ishrz <ishrzz66@gmail.com>
2025-10-29 12:37:50 +0530

    functions

---
+++ revision/Functions/test1.js
@@ -0,0 +1,21 @@
+// function temp( val1){
+//     console.log(`${val1} is a Brand`);
+// }
+
+// temp("honda");
+
+// let temp= function(v1){
+//     console.log(`${v1} is a brand`);
+// }
+
+// temp("Hero")
+
+let temp= (v1)=>{
+    console.log(`${v1} is a brand`);
+}
+
+temp("Kia");
+
+let temp2= (v2)=> console.log(`${v2} is a brand`);
+temp2("TATA");
+
