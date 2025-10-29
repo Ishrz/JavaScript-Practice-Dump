02cb4d755538d38de134da95af93c4c8ac73bc53
Ishrz <ishrzz66@gmail.com>
2025-10-29 17:14:22 +0530

    Arrays started

---
+++ revision/Arrays/array1.js
@@ -0,0 +1,57 @@
+// let arr3= new Array(1,5,3);
+// console.log(arr3);
+
+// let arr=[2,5,6,8,4];
+// let arr2=arr.map(function(val){
+//     return val*2;
+// });
+// console.log(arr2);
+
+// let arr=[2,5,6,8,4];
+//let arr2 = arr.map((val)=> val*2)
+//console.log(arr2);
+
+// let arr2=[2,5,6,8,4];
+// console.log(arr2);
+
+// arr2.push(100);
+// console.log(arr2);
+
+// arr2.pop();
+// console.log(arr2);
+
+// arr2.shift();
+// console.log(arr2);
+
+// arr2.unshift(600);
+// console.log(arr2);
+
+// arr2.splice(2,1);
+// console.log(`splice arr ${arr2}`);
+
+// let arrSlice=arr2.slice(0,arr2.length);
+// console.log(`slice arrSlice ${arrSlice}`);
+
+// let arr=[5,6,7,8,1,2,4];
+// console.log(arr);
+// arr.reverse();
+// console.log(arr);
+
+// let arrN=["shaikh",'khan',"zzzzz","ansari"];
+// arrN.reverse();
+// console.log(arrN);
+
+let arr=[5,7,6,1,4,9,4];
+console.log(arr);
+arr.sort();
+console.log(arr)
+
+let arrN= ["shaikh", "Aikh", "zaikh","Faikh"];
+console.log(arrN);
+
+// let newarrN=arrN.sort(function(a,b){
+//     a-b;
+// });
+// console.log(newarrN);
+
+
