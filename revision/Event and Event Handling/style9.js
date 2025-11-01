dc5d41d806fab93c9c632561298334a30d74ebef
Ishrz <ishrzz66@gmail.com>
2025-11-01 15:04:23 +0530

    event bubbling quetion solved

--- revision/Event and Event Handling/style9.js
+++ revision/Event and Event Handling/style9.js
@@ -13,4 +13,7 @@ nav.addEventListener("click", (evt)=>{
     alert("Nav is clicked");
 })
 
-//  hum output me nav par click karege to alert me aayga nav is click par agar nav ke kisi children par click karege to yani button ya a tags par bhi click karege to nav ka listener chal jayga aur nav is clicked aa jayga. due to event bubbling.
+let ul = document.querySelector("ul");
+ul.addEventListener("click", (evt)=>{
+    evt.target.classList.toggle("lt");
+})
