abbe740073c71c509a0255e4ed7195550b772ff4
Ishrz <ishrzz66@gmail.com>
2025-11-01 14:15:51 +0530

    event bubling example

--- revision/Event and Event Handling/style9.js
+++ revision/Event and Event Handling/style9.js
@@ -6,3 +6,11 @@ window.addEventListener("mousemove", (val)=>{
     abcd.style.top=val.clientY +"px";
     abcd.style.left=val.clientX +"px";
 })
+
+
+let nav = document.querySelector(".nav");
+nav.addEventListener("click", (evt)=>{
+    alert("Nav is clicked");
+})
+
+//  hum output me nav par click karege to alert me aayga nav is click par agar nav ke kisi children par click karege to yani button ya a tags par bhi click karege to nav ka listener chal jayga aur nav is clicked aa jayga. due to event bubbling.
