682b47d7e7675aa76cc858030e33a1337a16257c
Ishrz <ishrzz66@gmail.com>
2025-11-04 00:32:54 +0530

    local storage & session storage/coockies practice

---
+++ revision/local-storage-session storage/style.js
@@ -0,0 +1,13 @@
+localStorage.setItem("name","shaikh");
+
+let a=localStorage.getItem("name");
+console.log(a);
+
+localStorage.setItem("name","ahamad");
+a= localStorage.getItem("name");
+
+console.log(a);
+
+// localStorage.removeItem("name");
+
+sessionStorage.setItem("name","pokka");
