ec2afbde124b30f1d7926e784a8c4d29f8723a04
Ishrz <ishrzz66@gmail.com>
2025-11-10 16:55:17 +0530

    modified with clean code

--- revision/Callbacks-promises-Async-Await/script2.js
+++ revision/Callbacks-promises-Async-Await/script2.js
@@ -1,34 +1,31 @@
-function profile(userName, fnc){
-    console.log("fatching profile data...");
-    setTimeout( ()=>{
-        fnc({userName,_id:42, age:25, city:"pune"});
-    }, Math.floor(Math.random()*10)*1000
-)}
-
-function sarePostlekoAao(id, cb){
-        console.log("fatching profile posts....");
-
-    setTimeout( ()=>{
-
-        cb( {_id:id, posts:["p1","p2","p3","p4"]})
-
-    }, Math.floor(Math.random()*10)*1000)
+function profile(userName, fnc) {
+  console.log("fatching profile data...");
+  setTimeout(() => {
+    fnc({ userName, _id: 42, age: 25, city: "pune" });
+  }, Math.floor(Math.random() * 10) * 1000);
 }
 
-function savePostNikal(id, cb){
-    console.log("saving posts in array.....")
-    setTimeout( ()=>{
-        cb({_id:id, savePost:["saved1","saved2"]})
-    },4000)
+function sarePostlekoAao(id, cb) {
+  console.log("fatching profile posts....");
 
+  setTimeout(() => {
+    cb({ _id: id, posts: ["p1", "p2", "p3", "p4"] });
+  }, Math.floor(Math.random() * 10) * 1000);
 }
 
-profile("shaikh", function(user){
-    console.log(user);
-    sarePostlekoAao(user._id, function(posts){
+function savePostNikal(id, cb) {
+  console.log("saving posts in array.....");
+  setTimeout(() => {
+    cb({ _id: id, savePost: ["saved1", "saved2"] });
+  }, 4000);
+}
+
+profile("shaikh", function (user) {
+  console.log(user);
+  sarePostlekoAao(user._id, function (posts) {
     console.log(posts);
-    savePostNikal(user._id, function(saveP){
-        console.log(`saved posts : ${saveP.savePost}`)
-    })
+    savePostNikal(user._id, function (saveP) {
+      console.log(`saved posts : ${saveP.savePost}`);
     });
-})
+  });
+});
--- revision/Callbacks-promises-Async-Await/script3.js
+++ revision/Callbacks-promises-Async-Await/script3.js
@@ -1,16 +1,34 @@
 let pro = new Promise(function (resoldd, rejectt) {
-setTimeout( ()=> {
+  setTimeout(() => {
     let ran = Math.floor(Math.random() * 10);
-    (ran > 5)? resoldd(`${ran} : random number is grater then 5 condition true msg from resolved promise `) : rejectt(` ${ran} :random number is smaller then 5 condition fail msg from reject promise `)}, 3000);
+    if (ran > 5) {
+      resoldd(
+        `${ran} : random number is grater then 5 condition true msg from resolved promise `
+      );
+    } else {
+      rejectt(
+        ` ${ran} :random number is smaller then 5 condition fail msg from reject promise `
+      );
+    }
+  }, 3000);
 });
 
+//////////////////////
 pro
-.then((res)=> console.log(` .then function acpt val your code/data : ${res}`))
-.catch((rej)=> console.log(`.catch function acpt val code/data : ${rej}`))
-// pro
-// .then(function (res) {
-//   console.log(` .then function acpt val your code/data : ${res}`);
-// })
-// .catch(function (rej) {
-//   console.log(`.catch function acpt val code/data : ${rej}`);
+  .then(function (res) {
+    console.log(` .then function acpt val your code/data : ${res}`);
+  })
+  .catch(function (rej) {
+    console.log(`.catch function acpt val code/data : ${rej}`);
+  });
+  
+
+// let pro = new Promise(function (resoldd, rejectt) {
+// setTimeout( ()=> {
+//     let ran = Math.floor(Math.random() * 10);
+//     (ran > 5)? resoldd(`${ran} : random number is grater then 5 condition true msg from resolved promise `) : rejectt(` ${ran} :random number is smaller then 5 condition fail msg from reject promise `)}, 3000);
 // });
+
+// pro
+// .then((res)=> console.log(` .then function acpt val your code/data : ${res}`))
+// .catch((rej)=> console.log(`.catch function acpt val code/data : ${rej}`))
